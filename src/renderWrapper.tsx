54f87f58cb0356bd37cb78d802302445e7db5f06
Henric Trotzig <henric.trotzig@gmail.com>
2018-10-11 10:21:00 +0200

    Make renderWrapper.tsx compile
    The typescript compiler was trying to tell me that I had an unused
    import, and that the import couldn't be resolved. But happo was hiding
    that error so I had to dig in to the happo.io code and add some
    console logging.
    
    The bug is taken care of in https://github.com/happo/happo.io/pull/35.
    This commit will just bring the project to a good state again.

--- src/renderWrapper.tsx
+++ src/renderWrapper.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { ThemeProvider } from './core/theme/ThemeProvider';
 
 export default (component: React.ReactNode) => (
   <div style={{ border: '1px solid red', padding: 10 }}>{component}</div>
